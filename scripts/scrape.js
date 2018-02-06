eedd8a9cf533743c3948d4242fdedb2636c469b3
Derek Bocskey <bocskey.derek@gmail.com>
2018-02-05 23:50:13 -0500

    completed scrape script

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,30 +1,66 @@
-Make a request call to grab the HTML body from the site of your choice
-request("https://www.newegg.com/Product/ProductList.aspx?Submit=ENE&DEPA=0&Order=BESTMATCH&Description=1080ti&N=-1&isNodeId=1", function (error, response, html) {
+var axios = require("axios");
+var cheerio = require("cheerio");
 
-    // Load the HTML into cheerio and save it to a variable
-    // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-    var $ = cheerio.load(html);
+//scrape newegg for nvidia cards 
+var scrape = function () {
+    return axios.get("https://www.newegg.com/Product/ProductList.aspx?Submit=ENE&DEPA=0&Order=BESTMATCH&Description=1080ti&N=-1&isNodeId=1").then(function (res) {
 
-    // An empty array to save the data that we'll scrape
-    var results = [];
+        //set $ to cheerio shorthand
+        var $ = cheerio.load(res.data);
 
-    // Select each element in the HTML body from which you want information.
-    // NOTE: Cheerio selectors function similarly to jQuery's selectors,
-    // but be sure to visit the package's npm page to see how it works
-    $("a.item-title").each(function (i, element) {
+        //make array to store search results
+        var gpus = [];
 
-        var link = $(element).attr("href");
-        var title = $(element).text();
-        var price = $(element).parent().find("li.price-current").find("strong").text();
+        //sort thru search results
+        $("a.item-title").each(function (i, element) {
 
-        // Save these results in an object that we'll push into the results array we defined earlier
-        results.push({
-            title: title,
-            link: link,
-            price: price
+            var link = $(element).attr("href");
+            var title = $(element).text();
+            var price = $(element).parent().find("li.price-current").find("strong").text();
+
+            //push gpus to array
+            results.push({
+                title: title,
+                link: link,
+                price: price
+
+            });
+
         });
+        return gpus;
     });
+};
 
-    // Log the results once you've looped through each of the elements found with cheerio
-    console.log(results);
-});
+
+// //Make a request call to grab the HTML body from the site of your choice
+// ("https://www.newegg.com/Product/ProductList.aspx?Submit=ENE&DEPA=0&Order=BESTMATCH&Description=1080ti&N=-1&isNodeId=1", function (error, response, html) {
+
+//     // Load the HTML into cheerio and save it to a variable
+//     // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+//     var $ = cheerio.load(html);
+
+//     // An empty array to save the data that we'll scrape
+//     var results = [];
+
+//     // Select each element in the HTML body from which you want information.
+//     // NOTE: Cheerio selectors function similarly to jQuery's selectors,
+//     // but be sure to visit the package's npm page to see how it works
+//     $("a.item-title").each(function (i, element) {
+
+//         var link = $(element).attr("href");
+//         var title = $(element).text();
+//         var price = $(element).parent().find("li.price-current").find("strong").text();
+
+//         // Save these results in an object that we'll push into the results array we defined earlier
+//         results.push({
+//             title: title,
+//             link: link,
+//             price: price
+//         });
+//     });
+
+//     // Log the results once you've looped through each of the elements found with cheerio
+//     console.log(results);
+// });
+
+module.exports = scrape;
--- server.js
+++ server.js
@@ -5,24 +5,33 @@
 var express = require("express");
 var mongoose = require("mongoose");
 var router = require("./controllers/controller.js");
+var logger = require("morgan");
+var axios = require("axios");
+var cheerio = require("cheerio");
+var parser = require("body-parser");
 
 // Initialize Express
 var app = express();
 
+//configure ejs
+app.set('view engine', 'ejs');
+
 // Database configuration
 // Save the URL of our database as well as the name of our collection
-var databaseUrl = "newegg";
+var databaseUrl = "Newegg";
 var collections = ["products"];
 
+
+
 // Use mongoose to hook the database to the db variable
-// var db = mongoose(databaseUrl, collections);
+ var db = mongoose(databaseUrl, collections);
 
 
 
 // This makes sure that any errors are logged if mongodb runs into an issue
-// db.on("error", function(error) {
-//  console.log("Database Error:", error);
-// });
+db.on("error", function(error) {
+ console.log("Database Error:", error);
+});
 
 // TODO: Implement the remaining two routes
 
