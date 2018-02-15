a6d9b6255ca3469be7fdd67b2cabcca70308a72e
Derek Bocskey <bocskey.derek@gmail.com>
2018-02-14 23:36:11 -0500

    updated server file

--- server.js
+++ server.js
@@ -1,27 +1,45 @@
 // Dependencies                    
 var express = require("express");
+var exphbs = require("express-handlebars");
 var mongoose = require("mongoose");
-var router = require("./controllers/controller.js");
 var logger = require("morgan");
 var axios = require("axios");
 var cheerio = require("cheerio");
 var parser = require("body-parser");
+
+//set port
+var PORT = process.env.PORT || 8080;
+
+//Initiate app
 var app = express();
 
+//set public folder to static 
+app.use(express.static("public"));
+
 //scraper script 
-var scrape = require("./scripts/scrape.js");
+// var scrape = require("./scripts/scrape.js");
 
-//configure ejs
-app.set('view engine', 'ejs');
+// all requests go through route middleware
+app.use(routes);
 
-//test to see if working 
-app.get("/", function(req, res) {
-    res.send("Hello world");
-  });
 
-mongoose.connect("mongodb://localhost/gpuscraper");
+//connect handlebars 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set('view engine', 'handlebars');
+
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/Products";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
 
 // Set the app to listen on port 3000
 app.listen(8080, function() {
   console.log("App running on port 8080!");
 });
+
--- views/app.js
+++
@@ -1,9 +0,0 @@
-//Grab GPUs as a json
-$.getJSON("/gpus", function(data) {
-
-    //for each gpu
-    for (var i = 0; i < data.length; i++) {
-    //display the card titles
-    $("#gpus").append("<p") 
-    } 
-});
